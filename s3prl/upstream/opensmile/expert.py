1a5de2e206f68be4e409aa1e7ef03c0f693e9e61
Shinya Fujie <shinya.fujie@p.chibakoudai.jp>
2022-06-24 18:35:02 +0900

    quick fix for mismatch between output feature length and downsample setting.

--- s3prl/upstream/opensmile/expert.py
+++ s3prl/upstream/opensmile/expert.py
@@ -1,4 +1,5 @@
 from typing import List, Union, Dict
+import numpy as np
 import torch
 from torch.nn.utils.rnn import pad_sequence
 import opensmile
@@ -27,8 +28,13 @@ class UpstreamExpert(UpstreamBase):
         if len(wavs) > 0:
             device = wavs[0].device
         for wav in wavs:
-            r = self.smile.process_signal(wav.cpu(), SAMPLE_RATE)
-            feats.append(torch.tensor(r.to_numpy()))
+            result = self.smile.process_signal(wav.cpu(), SAMPLE_RATE)
+            result = result.to_numpy()
+            if result.shape[0] != 0 and result.shape[0] < len(wav) // self.downsample_rate - 2:
+                x = np.repeat(result[:1, :], (len(wav) // self.downsample_rate - 2) - result.shape[0], axis=0)
+                result = np.concatenate([x, result], axis=0)
+                # import ipdb; ipdb.set_trace()
+            feats.append(torch.tensor(result))
         padded_feats = pad_sequence(feats, batch_first=True)
         padded_feats = padded_feats.to(device)
         return {
